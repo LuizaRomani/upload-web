df2d48d6a309a4233be257976d49a848627af803
Luiza Sofal <lrsofal@sga.pucminas.br>
2025-03-12 14:41:47 -0300

    cancelamento e retentativa

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -3,20 +3,21 @@ import { UploadWidgetUploadItem } from './upload-widget-upload-item'
 
 export function UploadWidgetUploadList() {
   const uploads = useUploads(store => store.uploads)
+
   const isUploadListEmpty = uploads.size === 0
 
   return (
     <div className="px-3 flex flex-col gap-3">
       <span className="text-xs font-medium">
-        Uploaded Files <span className="text-zinc-400">(2)</span>
+        Uploaded files <span className="text-zinc-400">(2)</span>
       </span>
 
       {isUploadListEmpty ? (
-        <span className="text-xs text-zinc-400">No uplaods added</span>
+        <span className="text-xs text-zinc-400">No uploads added</span>
       ) : (
         <div className="flex flex-col gap-2">
           {Array.from(uploads).map(([uploadId, upload]) => {
-            return <UploadWidgetUploadItem key={uploadId} upload={upload} />
+            return <UploadWidgetUploadItem key={uploadId} upload={upload} uploadId={uploadId} />
           })}
         </div>
       )}
