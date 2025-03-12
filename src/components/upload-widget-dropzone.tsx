307a833bcf0f1c9ee2df60c4a0d720105d7a2e63
Luiza Sofal <lrsofal@sga.pucminas.br>
2025-03-12 15:09:16 -0300

    calculando uploads pending

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,12 +1,12 @@
 import { motion } from 'motion/react'
 import { useDropzone } from 'react-dropzone'
-import { useUploads } from '../store/uploads'
+import { usePendingUploads, useUploads } from '../store/uploads'
 import CircularProgressBar from './ui/circular-progress-bar'
 
 export function UploadWidgetDropzone() {
-  const { addUploads } = useUploads()
-  const isThereAnyPendingUpload = false
-  const uploadGlobalPercentage = 66
+  const addUploads = useUploads(store => store.addUploads)
+  const amountOfUploads = useUploads(store => store.uploads.size)
+  const { isThereAnyPendingUploads, globalPercentage } = usePendingUploads()
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: true,
@@ -19,17 +19,18 @@ export function UploadWidgetDropzone() {
     },
   })
   return (
-    <motion.div className="px-3 flex flex-col gap-3" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.6 }}>
+    <motion.div className="px-3 flex flex-col gap-3" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.2 }}>
       <div
         data-active={isDragActive}
-        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-400 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
+        className="cursor-pointer text-zinc-400 bg-black/20 p-5 rounded-lg border border-zinc-700 border-dashed h-32 flex flex-col items-center justify-center gap-1 hover:border-zinc-600 transition-colors data-[active=true]:bg-indigo-500/10 data-[active=true]:border-indigo-500"
         {...getRootProps()}
       >
         <input type="file" {...getInputProps()} />
-        {isThereAnyPendingUpload ? (
+
+        {isThereAnyPendingUploads ? (
           <div className="flex flex-col gap-2.5 items-center">
-            <CircularProgressBar progress={uploadGlobalPercentage} size={56} strokeWidth={4} />
-            <span className="text-xs">Uploading 2 files...</span>
+            <CircularProgressBar progress={globalPercentage} size={56} strokeWidth={4} />
+            <span className="text-xs">Uploading {amountOfUploads} files...</span>
           </div>
         ) : (
           <>
@@ -38,6 +39,7 @@ export function UploadWidgetDropzone() {
           </>
         )}
       </div>
+
       <span className="text-xxs text-zinc-400">Only PNG and JPG files are supported.</span>
     </motion.div>
   )
